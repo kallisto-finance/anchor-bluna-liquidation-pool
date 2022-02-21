f320501c0d7bc4019b9edb64d972a6845852c5c6
wc117 <williamchang89117@gmail.com>
2022-02-21 14:26:37 +0800

    Fix schema error

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use terra_deposit_withdraw::msg::{OwnerResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use terra_deposit_withdraw::msg::{ExecuteMsg, InstantiateMsg, OwnerResponse, QueryMsg};
 use terra_deposit_withdraw::state::State;
 
 fn main() {
